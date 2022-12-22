1043b3bb654b378bfe7174b60e958fc44390bc9f
Barruz <b.lyckova@gmail.com>
2022-12-22 17:27:06 +0100

    Initial Commit

---
+++ WayPicker/WayPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayPickerApp.swift
+//  WayPicker
+//
+//  Created by Barbora Růžičková on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WayPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
